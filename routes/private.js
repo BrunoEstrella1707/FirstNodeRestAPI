b53891f5d13dde072a571eb2ed593ff234bf9784
brunoestrella <brunogamer2002@gmail.com>
2026-01-22 19:53:38 -0300

    <feat: adding list users private route>

---
+++ routes/private.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+
+const router = express.Router()
+const prisma = new PrismaClient()
+
+
+router.get('/list', async (req, res) => {
+    try {
+
+        const users = await prisma.user.findMany({
+            omit: { password: true }
+        })
+
+        return res.status(200).json(users)
+
+    } catch (e) {
+        return res.status(400).json({message: `Error ${e}`})
+    }
+})
+
+export default router
