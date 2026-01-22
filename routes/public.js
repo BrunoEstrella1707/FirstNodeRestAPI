58a7ca9f034b2393cde2d951c956be978f3bdb69
brunoestrella <brunogamer2002@gmail.com>
2026-01-22 17:05:56 -0300

    <feat: adding login route, auth with jwt>

--- routes/public.js
+++ routes/public.js
@@ -1,10 +1,13 @@
-import bcrypt from 'bcrypt'
+import bcrypt, { compare } from 'bcrypt'
 import express from 'express'
+import jwt from 'jsonwebtoken'
 import { PrismaClient } from '@prisma/client'
 
 
 const router = express.Router()
 const prisma = new PrismaClient()
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 
 router.post('/register', async (req, res) => {
@@ -23,8 +26,44 @@ router.post('/register', async (req, res) => {
         })
 
         res.status(201).json({"message": "User created successfully!"})
+
     } catch(e) {
+        
         res.status(400).json({"message": `Invalid request ${e.message}`})
+
+    }
+})
+
+
+router.post('/login', async (req, res) => {
+    try {
+
+        const user_info = req.body
+        const user = await prisma.user.findUnique({
+            where: {
+                email: user_info.email,
+            }
+        })
+
+        if (!user) {
+            return res.status(404).json({message: "User not found."})
+        }
+
+        const isMatch = await bcrypt.compare(user_info.password, user.password)
+
+        if (!isMatch){
+            return res.status(404).json({message: "The password is incorrect."})
+        }
+
+        const token = jwt.sign({id: user.id},
+            JWT_SECRET,
+            { expiresIn: '1m'}
+        )
+
+        res.status(200).json({access: token})
+
+    } catch (e) {
+        res.status(400).json({message: `Invalid request ${e.message}`})
     }
 })
 
