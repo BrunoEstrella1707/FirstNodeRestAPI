5f16cdb3a839d99dbfe0ae352f67312fe09312d6
brunoestrella <brunogamer2002@gmail.com>
2026-01-22 19:52:51 -0300

    <feat: adding private routes and middlewares>

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import publicRoutes from './routes/public.js'
+import privateRoutes from './routes/private.js'
+import auth from './middlewares/auth.js'
 
 
 const app = express()
@@ -7,6 +9,7 @@ app.use(express.json())
 
 
 app.use('/user', publicRoutes)
+app.use('/user', auth, privateRoutes)
 
 
 app.listen(3000, () => console.log('Server running on port 3000 🚀...'))
