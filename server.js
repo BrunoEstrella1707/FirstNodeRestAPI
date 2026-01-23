e0bac5ac5c381876f1b8568a8a60337021d68de1
brunoestrella <brunogamer2002@gmail.com>
2026-01-23 09:21:41 -0300

    <feat: adding cookie-parser>

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ import express from 'express'
 import publicRoutes from './routes/public.js'
 import privateRoutes from './routes/private.js'
 import auth from './middlewares/auth.js'
+import cookieParser from 'cookie-parser'
 
 
 const app = express()
+
+
 app.use(express.json())
+app.use(cookieParser())
 
 
 app.use('/user', publicRoutes)
