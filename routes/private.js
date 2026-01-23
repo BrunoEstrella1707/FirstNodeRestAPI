dba6b6e1811168345c7528754f0b1ee5331c6cd7
brunoestrella <brunogamer2002@gmail.com>
2026-01-23 09:22:36 -0300

    <feat: adding detail user route>

--- routes/private.js
+++ routes/private.js
@@ -8,12 +8,30 @@ const prisma = new PrismaClient()
 
 router.get('/list', async (req, res) => {
     try {
+        console.log(req)
 
         const users = await prisma.user.findMany({
             omit: { password: true }
         })
 
         return res.status(200).json(users)
+
+    } catch (e) {
+        return res.status(400).json({message: `Error ${e}`})
+    }
+})
+
+
+router.get('/detail/:id', async (req, res) => {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                id: req.params.id
+            },
+            omit: {password: true}
+        })
+
+        return res.status(200).json(user)
 
     } catch (e) {
         return res.status(400).json({message: `Error ${e}`})
